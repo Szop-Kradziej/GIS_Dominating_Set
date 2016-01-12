c9c79c66b728ca3dee2a7c3381a40b51d1325489
Szop-kradziej <karolina.drabarz@gmail.com>
2016-01-12 16:52:05 +0100

    Remove prints

--- com/drabarz/karolina/ClassicalSetCoverageAlgorithm.py
+++ com/drabarz/karolina/ClassicalSetCoverageAlgorithm.py
@@ -2,18 +2,17 @@ import networkx as nx
 
 class ClassicalSetCoverageAlgorithm (object):
     def findDominatingSet(self, graph):
-        dominating_set = []
         print "CSCA"
-        coverage_sets = self.getCoverageSets(graph)
         
+        dominating_set = []
+        coverage_sets = self.getCoverageSets(graph)
         U = nx.nodes(graph)
+        
         while not len(U) == 0:
-            print U[0]
             max_covered_set = self.findMaxCoveredSet(U, coverage_sets)
             dominating_set = self.addSetToResult(dominating_set, max_covered_set)
             U = self.deleteNodes(U, max_covered_set)
-        
-        print len(coverage_sets)
+            
         return dominating_set;
     
     def getCoverageSets(self, graph):
@@ -28,13 +27,12 @@ class ClassicalSetCoverageAlgorithm (object):
         neighbors = graph.neighbors(node)
         coverage_set = neighbors
         coverage_set.append(node)
-        print coverage_set
         return coverage_set
     
     def findMaxCoveredSet(self, U, coverage_sets):
         max_covered_set = []
-        print "findMaxCoveredSet"
         max_sum = 0
+        
         for nodes_set in coverage_sets:
             sum = 0
             for node in nodes_set:
@@ -43,7 +41,7 @@ class ClassicalSetCoverageAlgorithm (object):
             if sum > max_sum:
                 max_sum = sum
                 max_covered_set = nodes_set
-            print "sum: ", sum, "max", max_sum
+                
         return max_covered_set
     
     def addSetToResult(self, dominating_set, max_covered_set):
