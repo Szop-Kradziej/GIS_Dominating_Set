51c3181c6ba03eb5f81ee460c4e82159b73baffb
Szop-kradziej <karolina.drabarz@gmail.com>
2016-01-12 20:38:09 +0100

    Implement ModifiedGreedyAlgorithm

--- com/drabarz/karolina/ModifiedGreedyAlgorithm.py
+++ com/drabarz/karolina/ModifiedGreedyAlgorithm.py
@@ -1,7 +1,34 @@
 import networkx as nx
+import copy
 
 class ModifiedGreedyAlgorithm (object):
     def findDominatingSet(self, graph):
-        dominatingSet = nx.dominating_set(graph);
         print "MGA"
-        return dominatingSet;
+        
+        modify_graph = copy.deepcopy(graph)
+        dominating_set = []
+        nodes = nx.nodes(modify_graph)
+        
+        while not len(nodes) == 0:
+            node = self.findMaxDegreeNode(modify_graph)
+            dominating_set.append(node)
+            modify_graph = self.removeNodeAndNeighbors(modify_graph, node)
+            nodes = nx.nodes(modify_graph)
+            
+        return dominating_set;
+    
+    def findMaxDegreeNode(self, graph):
+        nodes = nx.nodes(graph)
+        node = max(nodes, key=lambda node: nx.degree(graph, node))
+        return node
+    
+    def removeNodeAndNeighbors(self, graph, node):
+        new_graph = graph
+        neighbors = new_graph.neighbors(node)
+        
+        for neighbor in neighbors:
+            if neighbor in new_graph:
+                new_graph.remove_node(neighbor)
+                
+        new_graph.remove_node(node)
+        return new_graph
